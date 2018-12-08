8d8fb262498d67ab60d833e3862d1b3df8f0ff6d
robertbutacu <robertbutacu@gmail.com>
2018-12-08 19:17:27 +0000

    added implicit classes

--- src/main/scala/instances/NumeralRandomizerAuxiliary.scala
+++ src/main/scala/instances/NumeralRandomizerAuxiliary.scala
@@ -4,4 +4,8 @@ import typeclasses.NumeralRandomizerAuxiliary
 
 object NumeralRandomizerAuxiliary {
   def apply[A](implicit numeralRandomizerAuxiliary: NumeralRandomizerAuxiliary[A]): NumeralRandomizerAuxiliary[A] = numeralRandomizerAuxiliary
+
+  implicit class NumeralRandomizerAuxiliaryImplicit[A](input: A) {
+    def random(implicit numeralRandomizerAuxiliary: NumeralRandomizerAuxiliary[A]): A = numeralRandomizerAuxiliary.nextRandom
+  }
 }
--- src/main/scala/instances/RandomizerAuxiliary.scala
+++ src/main/scala/instances/RandomizerAuxiliary.scala
@@ -4,4 +4,8 @@ import typeclasses.RandomizerAuxiliary
 
 object RandomizerAuxiliary {
   def apply[A](implicit randomizerAuxiliary: RandomizerAuxiliary[A]): RandomizerAuxiliary[A] = randomizerAuxiliary
+
+  implicit class NumeralRandomizerAuxiliaryImplicit[A](input: A) {
+    def random(implicit randomizerAuxiliary: RandomizerAuxiliary[A]): A = randomizerAuxiliary.nextRandom
+  }
 }
