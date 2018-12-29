b80390ba961725b553691349bfa80d084fa1aa06
robertbutacu <robertbutacu@gmail.com>
2018-12-29 14:52:03 +0200

    added implicits for randomizer

--- src/main/scala/typeclasses/instances/RandomizerAuxiliary.scala
+++ src/main/scala/typeclasses/instances/RandomizerAuxiliary.scala
@@ -8,4 +8,16 @@ object RandomizerAuxiliary {
   implicit class NumeralRandomizerAuxiliaryImplicit[A](input: A) {
     def random(implicit randomizerAuxiliary: RandomizerAuxiliary[A]): A = randomizerAuxiliary.nextRandom
   }
+
+  def implicitDoubleRandomizer: RandomizerAuxiliary[Double] = new RandomizerAuxiliary[Double] {
+    override def nextRandom: Double = randomGenerator.nextDouble()
+  }
+
+  def implicitIntRandomizer: RandomizerAuxiliary[Int] = new RandomizerAuxiliary[Int] {
+    override def nextRandom: Int = randomGenerator.nextInt()
+  }
+
+  def implicitFloatRandomizer: RandomizerAuxiliary[Float] = new RandomizerAuxiliary[Float] {
+    override def nextRandom: Float = randomGenerator.nextFloat()
+  }
 }
